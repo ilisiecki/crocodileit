e73433a6ad258c9750d1c8dedbba7d6778229c9e
ilisiecki <igor.fox.lisiecki@gmail.com>
2023-11-22 14:16:01 +0100

    Create max-width-wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { MaxWidthWrapper } from "@/components/max-width-wrapper";
 
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <MaxWidthWrapper>
+      <div className="text-cetner mx-auto flex max-w-3xl flex-col items-start py-20"></div>
+    </MaxWidthWrapper>
+  );
 }
